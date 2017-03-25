0a150f1021cdf569ae76c386e731afe6558efb3b
Tyler-Zhang <tztylerzhang@gmail.com>
2017-03-25 19:04:34 -0400

    working on physical buttons

---
+++ Assets/Scripts/GetLeapFingers.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+using Leap;
+
+public class GetLeapFingers : MonoBehaviour 
+{
+	HandModel hand_model;
+	Hand leap_hand;
+
+	void Start() 
+	{
+		hand_model = gameObject.GetComponent<HandController>().rightGraphicsModel;
+		leap_hand = hand_model.GetLeapHand();
+		if (leap_hand == null) Debug.LogError("No leap_hand founded");
+	}
+
+	void Update() 
+	{
+		for (int i = 0; i < HandModel.NUM_FINGERS;i++)
+		{
+			FingerModel finger = hand_model.fingers[i];
+			// draw ray from finger tips (enable Gizmos in Game window to see)
+			Debug.DrawRay(finger.GetTipPosition(), finger.GetRay().direction, Color.red); 
+		}
+	}
+}
---
+++ Assets/Scripts/PhysicalButton.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PhysicalButton : MonoBehaviour {
+
+	public string action;
+
+	void OnTriggerEnter(Collider col){
+		Debug.Log ("collision");
+		if (col.transform.parent.gameObject.name == "middle" && col.name == "bone3") {
+
+			switch (action) {
+			case "challenge":
+				Debug.Log ("challenge");
+				break;
+			case "rockon":
+				Debug.Log ("Rock on");
+				break;
+			case "record":
+				Debug.Log ("record");
+				break;
+			}
+
+
+			Debug.Log ("touched");
+		}
+	}
+		
+}
