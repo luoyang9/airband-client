66057245b8af4ba9ca45cdb7e564718301f73cf6
Jiyu Zhang <me@tylerzhang.com>
2017-03-26 05:13:31 -0400

    rigged piano

--- Assets/HitKey.cs
+++ Assets/HitKey.cs
@@ -1,18 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using AssemblyCSharp;
 
-public class HitKey : MonoBehaviour {
+public class HitKey : PhysicalInstrument {
 
 	public AudioSource centerAudio;
-
+	public int NOTE_ID;
+	Main main;
 	// Use this for initialization
 	void Start () {
+		main = this.getMain ();
 	}
 
 	void OnTriggerEnter(Collider col) {
 		if(col.name == "bone3"  && col.transform.parent.gameObject.name == "middle") {
 			centerAudio.Play ();
+			main.noteHit (NOTE_ID, true);
 		}
 	}
 	
